f5aa5b6b4f0d48a49edfddc9c43216e38955072f
Lucas Teles <lucas.teles@lambda3.com.br>
2020-06-23 20:10:19 -0300

    set canvas

--- src/sketch.ts
+++ src/sketch.ts
@@ -7,8 +7,10 @@ import { setup, draw } from './app'
 const sketch = (p5: P5) => {
   window.p5 = p5
   p5.setup = () => {
-    const canvas = p5.createCanvas(200, 200)
+
+    const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight)
     canvas.parent("app")
+
     setup()
   }
 
