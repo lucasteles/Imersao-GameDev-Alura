ff23aceb6f4e6d4239ef90409812ca8aeb308a88
Lucas Teles <lucas.teles@lambda3.com.br>
2020-06-24 12:18:22 -0300

    ajuste versao

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,8 @@
-import MyCircle from './MyCircle'
-
-const myCircles: MyCircle[] = []
 
 export function setup() {
-
-    p5.background('white')
-
-    for (let i = 1; i < 4; i++) {
-      const p = p5.width / 4
-      const circlePos = p5.createVector(p * i, p5.height / 2)
-      const size = i % 2 !== 0 ? 24 : 32
-      myCircles.push(new MyCircle(circlePos, size))
-    }
+    p5.background('red')
 }
 
 export function draw() {
-    myCircles.forEach(circle => circle.draw())
-}
-
+  console.log(1)
+ } 
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,7 +1,6 @@
-import P5 from 'p5'
 import 'p5/lib/addons/p5.dom'
 import './styles.css'
-
+import P5 from 'p5'
 import { setup, draw } from './app'
 
 const sketch = (p5: P5) => {
